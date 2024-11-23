2ae8195f2c1178abe6710e935534947fc8ab1355
Balla Hemalatha <hemalathaballa5@gmail.com>
2024-11-23 13:27:24 +0530

    second commit

--- src/main/java/com/example/spring_jenkins_integration/SpringJenkinsIntegrationApplication.java
+++ src/main/java/com/example/spring_jenkins_integration/SpringJenkinsIntegrationApplication.java
@@ -1,13 +1,18 @@
 package com.example.spring_jenkins_integration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsIntegrationApplication {
 	
+	Logger logger = LoggerFactory.getLogger(SpringJenkinsIntegrationApplication.class);
+	
 	public void message() {
 		System.out.println("Application started...............");
+		logger.info("Application started...............");
 	}
 
 	public static void main(String[] args) {
